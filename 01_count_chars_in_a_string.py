3cdccb78388601346767f44fefa255d3eb916003
kzhelyazkov81 <kzhelyazkov81@gmail.com>
2022-04-28 15:48:58 +0300

    Initial commit

---
+++ 01_count_chars_in_a_string.py
@@ -0,0 +1,11 @@
+my_dict = {}
+word = input()
+
+for ch in word:
+    if ch not in my_dict and ch != ' ':
+        my_dict[ch] = 1
+    elif ch != ' ':
+        my_dict[ch] += 1
+
+for key, value in my_dict.items():
+    print(f'{key} -> {value}')
